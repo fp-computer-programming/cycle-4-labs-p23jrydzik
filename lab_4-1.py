926edc61e1fef77354a768b78cb8ed75984b9bde
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-10-11 10:25:39 -0400

    finished lab 4-1

---
+++ lab_4-1.py
@@ -0,0 +1,9 @@
+# author: JHR 10/11/22
+
+# Created variables for both names with inputs 
+first_name = input ("What is your first name?")
+last_name = input ("What is your last name?")
+
+# Added both strings together with a space then printed it
+full_name = first_name + ' ' + last_name
+print (full_name)
